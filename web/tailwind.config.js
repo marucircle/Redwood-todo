ba7e64a9ed6fff1275ce6eb45d64dd46ea50e5e8
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-02 18:06:58 +0900

    feat: tailwind.config.jsを仮作成

---
+++ web/tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        sp: '768px',
+        tb: '1200px',
+        pc: '9999px',
+      },
+      colors: {
+        white: '#f5f5f5',
+        black: '#333333',
+      },
+      fontSize: {
+        'small-text': '10px',
+        'medium-text': '12px',
+        'large-text': '14px',
+        'small-title': '16px',
+        'medium-title': '20px',
+        'large-title': '24px',
+      },
+      zIndex: {},
+      spacing: {
+        px: '1px',
+        0: '0',
+        2: '0.5rem',
+        4: '1rem',
+        8: '2rem',
+        12: '3rem',
+        16: '4rem',
+        20: '5rem',
+        24: '6rem',
+        32: '8rem',
+        40: '10rem',
+        48: '12rem',
+        56: '14rem',
+        64: '16rem',
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
