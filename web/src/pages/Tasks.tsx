5fa1a8eb9c595294e41bbfb9a02fff71da579e05
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-03 15:51:20 +0900

    feat: tasksページで想定していないURLの際のリダイレクトを作成

--- web/src/pages/Tasks.tsx
+++ web/src/pages/Tasks.tsx
@@ -1,7 +1,18 @@
-import { useParams } from '@redwoodjs/router'
+import { useParams, routes, Redirect } from '@redwoodjs/router'
+
+const taskViewMode = ['all', 'archived', 'completed'] as const
+
+type TaskViewMode = typeof taskViewMode[number]
+
+const isViewMode = (mode: string): mode is TaskViewMode => {
+  return taskViewMode.some((value) => value === mode)
+}
 
 const Tasks = () => {
   const { mode = 'all' } = useParams()
+  if (!isViewMode(mode)) {
+    return <Redirect to={routes.task()} />
+  }
   return <>{mode}ページです</>
 }
 
