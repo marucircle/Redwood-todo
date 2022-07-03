74211853f1ad5a1c252c3046ad1bd0099a8307cd
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-03 19:48:57 +0900

    feat: type追加

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,3 +1,21 @@
 import { taskViewMode } from './constants'
 
 export type TaskViewMode = typeof taskViewMode[number]
+
+export type Tag = {
+  id: number
+  name: string
+  bg_color: string
+  text_color: string
+  created_at: Date
+}
+
+export type Task = {
+  id: number
+  name: string
+  detail: string
+  tags: Tag[]
+  created_at: Date
+  is_checked: boolean
+  is_archived: boolean
+}
