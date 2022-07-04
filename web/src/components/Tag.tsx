40e2ea548ca496a8fa8ac90e5f2d7928c18365b4
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-05 07:58:17 +0900

    style: スタイル微修正

--- web/src/components/Tag.tsx
+++ web/src/components/Tag.tsx
@@ -8,7 +8,7 @@ export const Tag = ({ tag }: TagProps): JSX.Element => {
   return (
     <span
       style={{ backgroundColor: tag.bg_color, color: tag.text_color }}
-      className="px-4 py-2 rounded-full"
+      className="px-2 py-1 rounded-full text-medium-text"
     >
       {tag.name}
     </span>
--- web/src/components/TaskCard.tsx
+++ web/src/components/TaskCard.tsx
@@ -1,9 +1,26 @@
 import { Task } from 'src/types'
 
+import { Tag } from './Tag'
+
 export type TaskCardProps = {
   task: Task
 }
 
 export const TaskCard = ({ task }: TaskCardProps): JSX.Element => {
-  return <div>{task.name}</div>
+  return (
+    <div className="rounded-md bg-pure-white shadow-lg p-4 grid">
+      <div className="checkbox">checkbox</div>
+      <div className="text-medium-title font-bold pt-2">{task.name}</div>
+      <div className="py-2">
+        {task.tags.map((tag, index) => {
+          return (
+            <span key={index} className="pr-2">
+              <Tag tag={tag} />
+            </span>
+          )
+        })}
+      </div>
+      <div>test</div>
+    </div>
+  )
 }
