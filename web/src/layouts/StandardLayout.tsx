16e2a9344086f5b570b625d82c86e33a0fa2ef8e
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-14 07:41:43 +0900

    feat: StandardLayout作成

---
+++ web/src/layouts/StandardLayout.tsx
@@ -0,0 +1,18 @@
+type StandardLayoutProps = {
+  children?: React.ReactNode
+}
+
+const StandardLayout = ({ children }: StandardLayoutProps) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <header className="p-4 bg-pure-white">
+        <h1 className="text-large-title font-bold">Redwood Todo</h1>
+      </header>
+      <div className="">
+        <main className="flex-auto p-4 bg-white shadow-inner">{children}</main>
+      </div>
+    </div>
+  )
+}
+
+export default StandardLayout
