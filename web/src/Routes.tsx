bf895bea3d767fb9e320f3cb2cb8bc3c48bbf9c9
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-14 06:26:56 +0900

    fix: resolutionを追加して型エラー修正

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -19,11 +19,13 @@ import Tasks from './pages/Tasks'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/login" page={LoginPage} name="login" />
-      <Route path="/signup" page={SignupPage} name="signup" />
-      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
-      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      <Route notfound page={NotFoundPage} />
+      <Set>
+        <Route path="/login" page={LoginPage} name="login" />
+        <Route path="/signup" page={SignupPage} name="signup" />
+        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+        <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+        <Route notfound page={NotFoundPage} />
+      </Set>
       <Set wrap={[TodoLayout]}>
         <Route path="/tasks/{mode:String}" page={Tasks} name="tasks" />
         <Route path="/tasks" page={Tasks} name="task" />
