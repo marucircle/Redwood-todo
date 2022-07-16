88076bccb60fb3176422d7c3c34d882a5f7ba3bf
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-17 00:23:52 +0900

    feat: react-markdownのテスト

--- web/src/layouts/TodoLayout.tsx
+++ web/src/layouts/TodoLayout.tsx
@@ -44,13 +44,17 @@ const TodoLayout = ({ children }: TodoLayoutProps) => {
                 <span className="flex-auto">Completed</span>
               </Link>
             </li>
-            <li>
-              <div className="flex gap-x-4 px-4" onClick={logOut}>
-                <CheckOutlined />
-                <span>Logout</span>
-              </div>
-            </li>
           </ul>
+          <Link
+            to={routes.createTask()}
+            className="flex justify-center bg-info mx-8 py-2 my-16"
+          >
+            New Task
+          </Link>
+          <div className="flex gap-x-4 px-4 my-16" onClick={logOut}>
+            <CheckOutlined />
+            <span>Logout</span>
+          </div>
         </nav>
         <main className="flex-auto p-4 bg-white shadow-inner">{children}</main>
       </div>
--- web/src/pages/CreateTask.tsx
+++ web/src/pages/CreateTask.tsx
@@ -1,5 +1,41 @@
+import { useState } from 'react'
+
+import ReactMarkdown from 'react-markdown'
+
+import { Link, routes } from '@redwoodjs/router'
+
 const CreateTask = () => {
-  return <div></div>
+  const [isEdit, setIsEdit] = useState(true)
+  const [markdownData, setMarkdownData] = useState('')
+  return (
+    <div>
+      <div className="my-4">
+        <span className="bg-info px-4 mx-4 py-2">
+          <Link to={routes.createTask()}>Back</Link>
+        </span>
+      </div>
+      <div>
+        <div className="my-2">
+          <div className="mb-2">タスク内容</div>
+          <div onClick={() => setIsEdit(!isEdit)}>表示変更</div>
+          {isEdit ? (
+            <textarea
+              value={markdownData}
+              onChange={(e) => setMarkdownData(e.target.value)}
+              className="py-4 px-4 w-full"
+              rows={10}
+            ></textarea>
+          ) : (
+            <div className="bg-pure-white py-4 px-4">
+              <ReactMarkdown className="reactMarkDown">
+                {markdownData}
+              </ReactMarkdown>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CreateTask
