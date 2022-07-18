49b249b9810a8bdcc6464bfbfad00e3ba47319df
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-18 15:23:29 +0900

    feat: query作成

---
+++ web/src/graphql/tag.ts
@@ -0,0 +1,21 @@
+export const GET_TAGS = gql`
+  query GetTagQuery() {
+    tags {
+      id
+      name
+      bg_color
+      text_color
+    }
+  }
+`
+
+export const CREATE_TAG = gql`
+  mutation CreateTagMutation($input: CreateTagInput!) {
+    createTag(input: $input) {
+      id
+      name
+      bg_color
+      text_color
+    }
+  }
+`
