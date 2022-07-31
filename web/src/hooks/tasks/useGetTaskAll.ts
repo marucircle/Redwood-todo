ef299f1ad0a91231310724f1ae1bb8eba02a97de
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-07-31 16:56:39 +0900

    feat: タスク一覧取得用のhooks作成

---
+++ web/src/hooks/tasks/useGetTaskAll.ts
@@ -0,0 +1,20 @@
+import toast from 'react-hot-toast'
+
+import { useQuery } from '@redwoodjs/web'
+
+import { GET_TASKS } from 'src/graphql/task'
+import { Task } from 'src/types'
+
+export const useGetTaskAll = () => {
+  const {
+    data,
+    error: getTasksError,
+    loading: getTasksLoading,
+  } = useQuery<{ tasks?: Task[] }>(GET_TASKS, {
+    onError: () => {
+      toast.error('タスク一覧の取得に失敗しました...。')
+    },
+  })
+
+  return { tasks: data?.tasks, getTasksError, getTasksLoading }
+}
