27957138048999ce285fa38d3e36263d6a451dc6
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-08-11 11:36:14 +0900

    feat: GET_TASKクエリを作成

--- web/src/graphql/task.ts
+++ web/src/graphql/task.ts
@@ -18,6 +18,26 @@ export const GET_TASKS = gql`
   }
 `
 
+export const GET_TASK = gql`
+  query GetTaskQuery($id: Int!) {
+    task(id: $id) {
+      id
+      name
+      detail
+      priority
+      tags {
+        id
+        name
+        bg_color
+        text_color
+      }
+      is_checked
+      is_archived
+      created_at
+    }
+  }
+`
+
 export const CREATE_TASK = gql`
   mutation CreateTaskMutation($input: CreateTaskInput!) {
     createTask(input: $input) {
